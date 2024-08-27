8642b9ffe9ae4de4e5884606770d3a672c74b476
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-08-27 10:01:19 +0530

    init

---
+++ demo.py
@@ -0,0 +1,22 @@
+from hate.logger import logging
+from hate.exception import CustomException
+import sys
+from hate.configuration.gcloud_syncer import GCloudSync
+
+
+
+
+# obj = GCloudSync()
+# obj.sync_folder_from_gcloud("nl-hate-speech-repo","dataset.zip","data/dataset.zip")
+
+
+
+# logging.info("Hi my name is Asutosh sidhya")
+
+
+
+# try:
+#     a = 7 / "0"
+
+# except Exception as e:
+#     raise CustomException(e,sys) from e
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Asutosh sidhya",
+    author_email="ashutoshsidhya69@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
